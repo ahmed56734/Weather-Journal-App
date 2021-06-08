a73e59cc96547b9d447e5d975956c0703ac57155
ahmed ali <ahmed-abotaleb@outlook.com>
2021-06-08 09:46:54 +0200

    first submission

---
+++ server.js
@@ -0,0 +1,102 @@
+function getTemp() {
+    function getRandomIntInclusive(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
+    }
+
+    return getRandomIntInclusive(10, 45)
+}
+
+class WeatherEntry {
+    newDate = function () {
+        let d = new Date();
+        return d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
+    };
+
+    constructor(zipCode, content) {
+        this.date = this.newDate()
+        this.zipCode = zipCode
+        this.content = content
+        this.temp = getTemp()
+    }
+}
+
+const data = []
+const API_KEY = "A81kaskkajd18913asdkkasdd1j31iuja132"
+
+projectData = {
+    "message": "data fetched successfully",
+    "data": data,
+};
+
+
+// Require Express to run server and routes
+const express = require('express');
+
+// Start up an instance of app
+const app = express();
+
+/* Middleware*/
+//Here we are configuring express to use body-parser as middle-ware.
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+// Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
+
+// Initialize the main project folder
+app.use(express.static('website'));
+
+
+// Setup Server
+const port = 3000;
+const server = app.listen(port, listening);
+
+function listening() {
+    // console.log(server);
+    console.log(`running on http://localhost:${port}`);
+}
+
+
+app.get('/api/weather', function (request, response) {
+    console.log("get request =>", "\nurl " + request.url, "\nAuth " + request.header("Authorization"))
+    console.log('===============================================================================\n')
+
+
+    const authorization = request.header("Authorization")
+
+    if (authorization !== API_KEY) {
+        console.log("Unauthorized request")
+        response.status(401)
+        response.send()
+        return
+    }
+    response.send(projectData);
+});
+
+app.post('/api/weather', function (request, response) {
+    console.log("post request =>", "\nurl " + request.url, "\nbody " + JSON.stringify(request.body), "\nAuth " + request.header("Authorization"))
+    console.log('===============================================================================\n')
+
+    const authorization = request.header("Authorization")
+
+    if (authorization !== API_KEY) {
+        console.log("Unauthorized request")
+        response.status(401)
+        response.send()
+        return
+    }
+
+    const body = request.body
+    const zipCode = body.zipCode
+    const content = body.content
+
+    const weatherEntry = new WeatherEntry(zipCode, content)
+    data.push(weatherEntry)
+    response.send(projectData)
+})
+
+
---
+++ website/app.js
@@ -0,0 +1,97 @@
+/* Global Variables */
+const API_KEY = "A81kaskkajd18913asdkkasdd1j31iuja132"
+
+// Create a new date instance dynamically with JS
+
+
+class WeatherEntry {
+    constructor(zipCode, content) {
+        this.date = newDate()
+        this.zipCode = zipCode
+        this.conent = content
+    }
+}
+
+
+const postRequest = async (url = '', data = {}) => {
+    console.log(data)
+    const response = await fetch(url, {
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        credentials: 'same-origin', // include, *same-origin, omit
+        headers: {
+            'Content-Type': 'application/json',
+            "Authorization": API_KEY
+        },
+        body: JSON.stringify(data), // body data type must match "Content-Type" header
+    });
+
+    try {
+        return await response.json();
+    } catch (error) {
+        console.log("error", error);
+        // appropriately handle the error
+    }
+}
+
+const getRequest = async (url = '') => {
+    const response = await fetch(url, {
+        method: 'GET', // *GET, POST, PUT, DELETE, etc.
+        credentials: 'same-origin', // include, *same-origin, omit
+        headers: {
+            'Content-Type': 'application/json',
+            "Authorization": API_KEY
+        },
+    });
+
+    try {
+        return await response.json();
+    } catch (error) {
+        console.log("error", error);
+        // appropriately handle the error
+    }
+}
+
+async function getAllData() {
+    const response = await getRequest("api/weather")
+    updateUI(response.data)
+}
+
+async function addWeatherEntry(body) {
+    const response = await postRequest("api/weather", body)
+    updateUI(response.data)
+}
+
+function updateUI(data) {
+    console.log(data)
+    if (data.length === 0) {
+        return
+    }
+
+    const lastEntry = data[data.length - 1]
+
+    console.log(lastEntry)
+
+    const entryHolderElement = document.getElementById("entryHolder")
+    const dateElement = document.getElementById("date")
+    const tempElement = document.getElementById("temp")
+    const contentElement = document.getElementById("content")
+
+    dateElement.textContent = lastEntry.date
+    tempElement.textContent = lastEntry.temp
+    contentElement.textContent = lastEntry.content
+}
+
+
+function main() {
+    getAllData()
+
+    document.getElementById("generate").onclick = ev => {
+        const zip = document.getElementById("zip").value
+        const feeling = document.getElementById("feelings").value
+        document.getElementById("zip").value = ""
+        document.getElementById("feelings").value = ""
+
+        const requestBody = {zipCode: zip, content: feeling}
+        addWeatherEntry(requestBody)
+    }
+}
